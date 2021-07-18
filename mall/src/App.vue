22bf245f91092f2d35b8ef5657393103273eb743
guoguo <806849175@qq.com>
2021-07-18 11:33:33 +0800

    03-首页创建

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-
     <router-view></router-view>
   </div>
 </template>
@@ -14,21 +13,17 @@ export default {
   },
   data(){
     return{
-
+      res:{}
     }
   },
   mounted() {
+    this.axios.get('/user/login').then((res)=>{
+      this.res=res;
+    });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ @import "./assets/scss/reset.scss";
 </style>
